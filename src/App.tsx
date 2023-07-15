dd7f7aa1f9b7632c74692ee6e4e3267076382fb9
Brendan McGuirk <bwm1002@wildcats.unh.edu>
2023-07-15 10:10:59 -0400

    Rendered page (still broken)

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,26 @@
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
 import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  )
+    return (
+        <>
+            <header>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Projects</a></li>
+                </ul>
+            </header>
+            <main>
+                <h1>Brendan<br/><span className="highlight">McGuirk</span></h1>
+                <div className="about">
+                    Hi! My name is Brendan!<br/>I am a <span className="highlight">full-stack developer</span> working with React/Redux and many different back-ends with nearly <span className="highlight">half a decade of experience</span>.
+                </div>
+            </main>
+            <footer>
+                <a href="https://www.linkedin.com/in/brendan-w-mcguirk/" className="fa fa-linkedin"></a>
+                <a href="https://github.com/BlastMcGuirk" className="fa fa-github"></a>
+            </footer>
+        </>
+    )
 }
 
 export default App
--- src/features/projects/Projects.tsx
+++ src/features/projects/Projects.tsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from "react"
-
+import { useEffect } from "react"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
-import {
-    projectsSlice,
-} from "./projectsSlice"
 import styles from "./Projects.module.css"
 import { fetchProjectsAsync } from "./projectsActions";
 
@@ -37,53 +33,5 @@ export function Projects() {
                 return <p key={project.url} className="project">{project.name}</p>
             })}
         </div>
-    )
-
-  return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
-  )
+    );
 }
