0eb0d7f8978b5b13bfdd82f824aa0e50f8ad50d8
Brendan McGuirk <bwm1002@wildcats.unh.edu>
2023-07-16 19:09:37 -0400

    Setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
         <>
             <header>
                 <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/projects">Projects</a></li>
                 </ul>
             </header>
             <main>
                 <h1>Brendan<br/><span className="highlight">McGuirk</span></h1>
                 <div className="about">
-                    Hi! My name is Brendan!<br/>I am a <span className="highlight">full-stack developer</span> working with React/Redux and many different back-ends with nearly <span className="highlight">half a decade of experience</span>.
+                    Hi! My name is Brendan!<br/>I am a <span className="highlight">full-stack developer</span> working with React/Redux and many different back-ends with <span className="highlight">4+ years of experience</span>.
                 </div>
             </main>
             <footer>
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,24 @@ import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { About } from "./about/About"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/about",
+    element: <About />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
